cb1cfe496765a15336930fb3099b55cfd71a8405
Per Stark <perstark.se@gmail.com>
2023-12-31 22:13:27 +0100

    init

--- src/data/resume-data.tsx
+++ src/data/resume-data.tsx
@@ -20,259 +20,145 @@ import {
 import { GitHubIcon, LinkedInIcon, XIcon } from "@/components/icons";
 
 export const RESUME_DATA = {
-  name: "Bartosz Jarocki",
-  initials: "BJ",
-  location: "Wrocław, Poland, CET",
-  locationLink: "https://www.google.com/maps/place/Wrocław",
+  name: "Per Stark",
+  initials: "PS",
+  location: "Örebro, Sweden, CET",
+  locationLink: "https://www.google.com/maps/place/Orebro",
   about:
-    "Full Stack Engineer focused on building products with extra attention to detail",
+  "A builder at heart and a psychologist by training. Interested in all sorts of things, always enjoying a challenge. My hands-on approach is driven by a passion for creating and problem-solving.",
   summary:
-    "As a Full Stack Engineer, I have successfully taken multiple products from 0 to 1. I lead teams effectively, ensuring an environment where people can do their best work. Currently, I work mostly with TypeScript, React, Node.js, and GraphQL. I have over 8 years of experience in working remotely with companies all around the world.",
-  avatarUrl: "https://avatars.githubusercontent.com/u/1017620?v=4",
-  personalWebsiteUrl: "https://jarocki.me",
+  "Equally invested in the fields of technology and social sciences, I channel my dual expertise into impactful community building and political engagement, serving as a member of the social welfare board. My work is informed by an intrinsic motivation to foster communal ties and drive societal progress through both my technical projects and political endeavors.",
+  avatarUrl: "https://avatars.githubusercontent.com/u/yourusername?v=4",
+  personalWebsiteUrl: "https://perstark.xyz",
   contact: {
-    email: "bartosz.jarocki@hey.com",
-    tel: "+48530213401",
+    email: "perstark.se@gmail.com",
+    tel: "+4670276752",
     social: [
       {
         name: "GitHub",
-        url: "https://github.com/BartoszJarocki",
+        url: "https://github.com/perstarkse",
         icon: GitHubIcon,
       },
       {
         name: "LinkedIn",
-        url: "https://www.linkedin.com/in/bjarocki/",
+        url: "https://www.linkedin.com/in/per-stark/",
         icon: LinkedInIcon,
-      },
-      {
-        name: "X",
-        url: "https://x.com/BartoszJarocki",
-        icon: XIcon,
-      },
+      }
     ],
-  },
+    },
   education: [
     {
-      school: "Wrocław University of Technology",
-      degree: "Bachelor's Degree in Control systems engineering and Robotics",
-      start: "2007",
-      end: "2010",
+      school: "Örebro Universitet",
+      degree: "Psychologist degree, Masters",
+      start: "2011",
+      end: "2016",
+    },
+    {
+      school: "EC Utbildningar",
+      degree: "Fullstack .NET Developer",
+      start: "2022",
+      end: "Current",
+    },
+    {
+      school: "Udemy",
+      degree: "Ethereum Blockchain Developer Bootcamp With Solidity",
+      start: "2022",
+      end: "2022",
     },
   ],
   work: [
     {
-      company: "Parabol",
-      link: "https://parabol.co",
-      badges: ["Remote"],
-      title: "Senior Full Stack Developer",
-      logo: ParabolLogo,
-      start: "2021",
-      end: "2024",
+      company: "Region Örebro Län",
+      link: "https://regionorebrolan.se",
+      badges: ["Certified Psychologist", "Personality assessment", "CBT"],
+      title: "Certified Psychologist",
+      // logo: AmbitLogo, // Placeholder
+      start: "2018",
+      end: "2023",
       description:
-        "Implemented new features, led squad, worked on improving the way developers ship the code, started migration from Emotion to Tailwind CSS and more. Technologies: React, TypeScript, GraphQL",
+        "Specialized in psychological assessment and evidence-based psychological treatment, primarily CBT. Development of control regarding flow and work around patients with mental illness.",
     },
     {
-      company: "Clevertech",
-      link: "https://clevertech.biz",
-      badges: ["Remote"],
-      title: "Lead Android Developer → Full Stack Developer",
-      logo: ClevertechLogo,
-      start: "2015",
-      end: "2021",
+      company: "Region Västra Götaland",
+      link: "https://vgregion.se",
+      badges: ["Neuropsychiatric assessment", "CBT"],
+      title: "Certified Psychologist",
+      // logo: AmbitLogo, // Placeholder
+      start: "2017",
+      end: "2018",
       description:
-        "Created Android mobile apps and led teams for companies like Vision Media, DKMS, or AAA. Built live streaming application for Evercast from scratch. Technologies: Android, Kotlin, React, TypeScript, GraphQL",
+        "Psychological assessment and evidence-based psychological treatment. Specialization regarding trauma related disorders as well as neuropsychiatric disorders.",
     },
     {
-      company: "Jojo Mobile",
-      link: "https://bsgroup.eu/",
-      badges: [],
-      title: "Android Developer → Lead Android Developer",
-      logo: JojoMobileLogo,
-      start: "2012",
-      end: "2015",
+      company: "TIQQE AB",
+      link: "https://tiqqe.com",
+      badges: ["Remote", "IT Consulting", "AWS Partner", "Serverless"],
+      title: "Software Developer",
+      // logo: BarepapersLogo, // Placeholder
+      start: "2022",
+      end: "Current",
       description:
-        "Built an Android team, created Android apps for biggest Polish companies like LOT, Polskie Radio, Agora, PolskaPress",
-    },
-    {
-      company: "Nokia Siemens Networks",
-      link: "https://www.nokia.com",
-      badges: [],
-      title: "C/C++ Developer",
-      logo: NSNLogo,
-      start: "2010",
-      end: "2012",
-      description: "Creating and testing software for LTE base stations",
+        "Software development of serverless applications in AWS using TypeScript and Vue. Using AI to solve real problems for clients.",
     },
   ],
-  skills: [
-    "JavaScript",
-    "TypeScript",
-    "React/Next.js/Remix",
-    "Node.js",
-    "GraphQL",
-    "Relay",
-    "WebRTC",
-  ],
-  projects: [
+  skills: 
+    [
+      "TypeScript",
+      "JavaScript",
+      "Large language models",
+      "Python",
+      "C#",
+      ".NET",
+      "Vue",
+      "Solidity",
+      "Psychological Assessments",
+      "Evidence-based Psychological Treatment",
+      "CBT",
+      "Neuropsychiatric Assessments",
+    ],
+    projects: [
     {
-      title: "Consultly",
+      title: "Student Sentiment Analysis",
       techStack: [
         "Side Project",
+        "Python",
         "TypeScript",
-        "Next.js",
-        "Vite",
-        "GraphQL",
-        "WebRTC",
+        "Vue",
+        "Large language models",
       ],
-      description: "A platform to build and grow your online business",
-      logo: ConsultlyLogo,
+      description: "Tracking student sentiment in discord chat and analyzing with regard to psychological variables pertaining stress.",
+      logo: TastyCloudLogo, // Placeholder
       link: {
-        label: "consultly.com",
-        href: "https://consultly.com/",
+        label: "sentiment",
+        href: "https://sentiment.perstark.xyz/",
       },
     },
     {
-      title: "Monito",
-      techStack: ["Side Project", "TypeScript", "Next.js", "Browser Extension"],
-      description:
-        "Browser extension that records everything happening in a web application",
-      logo: MonitoLogo,
-      link: {
-        label: "monito.dev",
-        href: "https://monito.dev/",
-      },
-    },
-    {
-      title: "Jarocki.me",
-      techStack: ["Side Project", "Next.js", "MDX"],
-      description:
-        "My personal website and blog. Built with Next.js and Notion API",
-      logo: JarockiMeLogo,
-      link: {
-        label: "github.com",
-        href: "https://jarocki.me/",
-      },
-    },
-    {
-      title: "Minimal",
-      techStack: ["Side Project", "Next.js", "Puppeteer"],
-      description:
-        "Minimalist calendars, habit trackers and planners generator",
-      logo: Minimal,
-      link: {
-        label: "useminimal.com",
-        href: "https://useminimal.com/",
-      },
-    },
-    {
-      title: "Barepapers",
-      techStack: ["Side Project", "Next.js", "Puppeteer"],
-      description:
-        "Generates beautiful wallpapers using random shapes and gradients",
-      logo: BarepapersLogo,
-      link: {
-        label: "barepapers.com",
-        href: "https://barepapers.com/",
-      },
-    },
-    {
-      title: "Year progress",
-      techStack: ["Side Project", "TypeScript", "Next.js"],
-      description: "Tracks current year progress and displays a countdown",
-      logo: YearProgressLogo,
-      link: {
-        label: "getyearprogress.com",
-        href: "https://getyearprogress.com/",
-      },
-    },
-    {
-      title: "Parabol",
+      title: "AI frontend Lekebergs kommun invoices",
       techStack: [
-        "Full Stack Developer",
+        "Side Project",
+        "Python",
         "TypeScript",
-        "React",
-        "Node.js",
-        "GraphQL",
+        "Vue",
+        "Large language models",
       ],
-      description:
-        "The Agile meeting co-pilot that delivers better meetings with less effort",
-      logo: ParabolLogo,
+      description: "Using LLMs to provide a frontend to a dataset containing Lekebergs kommun invoices.",
+      logo: TastyCloudLogo, // Placeholder
       link: {
-        label: "github.com",
-        href: "https://parabol.co/",
+        label: "Lekberg Invoices",
+        href: "https://lekebergsfakturor.perstark.xyz/",
       },
     },
     {
-      title: "Evercast",
-      techStack: [
-        "Lead Frontend Developer",
-
-        "TypeScript",
-        "React",
-        "Node.js",
-        "GraphQL",
-      ],
-      description:
-        "Creative collaboration platform that combines video conferencing and HD media streaming",
-      logo: EvercastLogo,
+      title: "Personal Website (legacy)",
+      techStack: ["Side Project", "TypeScript", "React", "Solidity", "Blockchain"],
+      description: "Personal website, including a NFT mint with generated profile pictures, also showcasing old projects.",
+      logo: YearProgressLogo, // Placeholder
       link: {
-        label: "evercast.us",
-        href: "https://www.evercast.us/",
+        label: "legacy",
+        href: "https://perstark.xyz/",
       },
-    },
-    {
-      title: "Mobile Vikings",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Android application for leading virtual mobile operator in Poland",
-      logo: MobileVikingsLogo,
-      link: {
-        label: "mobilevikings.pl",
-        href: "https://mobilevikings.pl/",
-      },
-    },
-    {
-      title: "Howdy",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Howdy is a place for you to join communities you care about",
-      logo: Howdy,
-      link: {
-        label: "play.google.com",
-        href: "https://howdy.co/",
-      },
-    },
-    {
-      title: "Tastycloud",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Android application for managing and displaying restaurant menus in kiosk mode",
-      logo: TastyCloudLogo,
-      link: {
-        label: "tastycloud.fr",
-        href: "https://www.tastycloud.fr/",
-      },
-    },
-    {
-      title: "Ambit",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Android application that helps with sharing your contact details",
-      logo: AmbitLogo,
-    },
-    {
-      title: "Bim",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Android application that helps with booking a table in a restaurants",
-      logo: BimLogo,
-    },
-    {
-      title: "Canal Digital GO",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Video streaming mobile application for Canal Digital subscribers",
-      logo: CDGOLogo,
-    },
+    }
+    // ... Add other projects here
   ],
-} as const;
+} as const; 
